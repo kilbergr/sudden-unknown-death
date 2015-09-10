e251c344ed885d996459ed26d8a7e896fbfe7dc5
kilbergr <rekilberg@gmail.com>
2015-09-10 14:57:37 -0700

    cycles through all possible combos

--- selenium.rb
+++ selenium.rb
@@ -27,47 +27,88 @@ if form.displayed?
 	# Will always choose the same age segmentation pattern
 	driver.find_element(:xpath, "//input[contains (@value, 'lcd3age')]").click
 	
-	stateOpt = Selenium::WebDriver::Support::Select.new(state)
-	def allStates(stateOpt)
+
+	def allStates(state, race, sex, year1, year2, ethnicity)
+		stateOpt = Selenium::WebDriver::Support::Select.new(state)
 		stNum = 1
 		until stNum == 57
 			if stNum < 10
 				chosenSt = stateOpt.select_by(:value, "0"+stNum.to_s)
-				if chosenSt.nil?
-					stNum+1
-				else
-					chooseRace(race)
-					setYr(year1, year2)
-
-				end
 			else 
-				stateOpt.select_by(:value, stNum.to_s)
+				chosenSt = stateOpt.select_by(:value, stNum.to_s)
+			end
+
+			if chosenSt.nil?
+				stNum+=1
+			else
+				# nesting race (leads to nested sex, ethnicity, year choices)
+				chooseRace(race, sex, year1, year2, ethnicity)
+				stNum+=1	
 			end 
-			stNum+=1	
 		end
 	end
 
-	def chooseRace(race)
+	def chooseRace(race, sex, year1, year2, ethnicity)
 		raceOpt = Selenium::WebDriver::Support::Select.new(race)
 		chosenRaceVal = 1
 		while chosenRaceVal < 6
 			raceOpt.select_by(:value, chosenRaceVal.to_s)
+			# nesting set sex
+			chooseSex(sex, year1, year2, ethnicity)
 			chosenRaceVal+=1
 		end
 	end
-		
+
+	def chooseSex(sex, year1, year2, ethnicity)
+		sexOpt = Selenium::WebDriver::Support::Select.new(sex)
+		chosenSexVal = 1
+		while chosenSexVal < 3
+			sexOpt.select_by(:value, chosenSexVal.to_s)
+			#nesting choose ethnicity
+			chooseEth(year1, year2, ethnicity)
+			chosenSexVal+=1
+		end
+	end
+	def chooseEth(year1, year2, ethnicity)
+		ethnicityOpt = Selenium::WebDriver::Support::Select.new(ethnicity)
+		chosenEthVal = 1
+		while chosenEthVal < 3
+			ethnicityOpt.select_by(:value, chosenEthVal.to_s)
+			# nesting set year
+			setYear(year1, year2)
+			chosenEthVal +=1
+		end
+	end
+
+	def setYear(year1, year2)
+		yearOpt1 = Selenium::WebDriver::Support::Select.new(year1)
+		yearOpt2 = Selenium::WebDriver::Support::Select.new(year2)
+		chosenYear = 1999
+		while chosenYear < 2014
+			yearOpt1.select_by(:value, chosenYear.to_s)
+			yearOpt2.select_by(:value, chosenYear.to_s)
+			# submitQ()
+			chosenYear+=1
+		end
+	end
 	
-	raceOpt = Selenium::WebDriver::Support::Select.new(race)
-	raceOpt.select_by(:value, "1")
-	sexOpt = Selenium::WebDriver::Support::Select.new(sex)
-	sexOpt.select_by(:value, "1")
-	yearOpt1 = Selenium::WebDriver::Support::Select.new(year1)
-	yearOpt1.select_by(:value, "1999")
-	yearOpt2 = Selenium::WebDriver::Support::Select.new(year2)
-	yearOpt2.select_by(:value, "1999")
-	request = driver.find_element(:name, "Submit")
-	request.submit
-end
+	def submitQ()
+		request = driver.find_element(:name, "Submit")
+		request.submit
+	end
+
+	allStates(state, race, sex, year1, year2, ethnicity)
+	
+	
+	# raceOpt = Selenium::WebDriver::Support::Select.new(race)
+	# raceOpt.select_by(:value, "1")
+	# sexOpt = Selenium::WebDriver::Support::Select.new(sex)
+	# sexOpt.select_by(:value, "1")
+	# yearOpt1 = Selenium::WebDriver::Support::Select.new(year1)
+	# yearOpt1.select_by(:value, "1999")
+	# yearOpt2 = Selenium::WebDriver::Support::Select.new(year2)
+	# yearOpt2.select_by(:value, "1999")
 
 download = driver.find_element(:link, "Download Results in a Spreadsheet (CSV) File").click
-p download
+
+end
